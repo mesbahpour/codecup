b7a955dd48a476911e2c0bf2d3685a7c7bad71e9
mesbahpour <mesbahpour.a@gmail.com>
2023-12-13 22:30:09 +0330

    Add Caesar cipher script

---
+++ 182545/main.js
@@ -0,0 +1,50 @@
+function encrypt(str, n) {
+  //encrypt function
+
+  let result = "";
+  
+  for(let i = 0; i < str.length; i++) {
+
+    let charCode = str.charCodeAt(i);
+    
+    if(charCode >= 65 && charCode <= 90) {
+      result += String.fromCharCode((charCode - 65 + n) % 26 + 65);
+    }
+    else if(charCode >= 97 && charCode <= 122) {
+      result += String.fromCharCode((charCode - 97 + n) % 26 + 97);
+    }
+    else {
+      result += str[i];
+    }
+  }
+
+  return result;
+}
+
+function decrypt(str, n) {
+  //decrypt function
+
+  let result = "";
+  
+  for(let i = 0; i < str.length; i++) {
+
+    let charCode = str.charCodeAt(i);
+    
+    if(charCode >= 65 && charCode <= 90) {
+      result += String.fromCharCode((charCode - 65 - n) % 26 + 65);
+    }
+    else if(charCode >= 97 && charCode <= 122) {
+      result += String.fromCharCode((charCode - 97 - n) % 26 + 97);
+    }
+    else {
+      result += str[i];
+    }
+  }
+
+  return result;
+}
+
+console.log(encrypt('abc', 6));
+console.log(decrypt('ghi', 6));
+
+export { encrypt, decrypt };
