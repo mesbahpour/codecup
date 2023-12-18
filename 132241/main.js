8f890c153accbca8c1b4c39173b72d0c37d87950
mesbahpour <mesbahpour.a@gmail.com>
2023-12-18 22:28:31 +0330

    Update main.js

--- 132241/main.js
+++ 132241/main.js
@@ -1,3 +1,17 @@
 document.querySelector(".btn").addEventListener("click", () => {
   //Your Code
+
+  const input = document.querySelector('.input').value;
+  const result = document.querySelector('.result');
+
+  let arr = input.split("");
+  let dots = [];
+
+  arr.forEach(element => {
+    dots.push(+obj[element])
+  });
+
+  const sum = dots.reduce((partialSum, a) => partialSum + a, 0);
+
+  result.innerHTML = sum
 });
